934e856c9c1ae543972776f66d6bd491efb85f67
Anna Matskevich <asm160704@gmail.com>
2024-09-25 19:44:17 +0300

    first commit

--- src/test/java/com/example/LionParamTest.java
+++ src/test/java/com/example/LionParamTest.java
@@ -1,2 +1,37 @@
-package com.example;public class LionParamTest {
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class LionParamTest {
+
+    private final boolean hasMane;
+    private final String sex;
+
+    public LionParamTest(String sex, boolean hasMane) {
+        this.sex = sex;
+        this.hasMane = hasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false}
+        };
+    }
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void lionConstructorTest() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        assertEquals(hasMane, lion.hasMane);
+    }
 }
--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,2 +1,46 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
+
+    @Mock
+    Feline feline;
+    Lion lion;
+
+
+    @Before
+    public void setUp() throws Exception {
+        lion = new Lion("Самец", feline);
+    }
+
+    @Test
+    public void getKittensTest() {
+        lion.getKittens();
+        Mockito.verify(feline).getKittens();
+    }
+
+    @Test
+    public void doesHaveManeTest() {
+        Assert.assertTrue(lion.doesHaveMane());
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        lion.getFood();
+        Mockito.verify(feline).getFood("Хищник");
+    }
+
+    @Test(expected = Exception.class)
+    public void ExceptionTest() throws Exception {
+        Lion lion = new Lion("Не самец и не самка", feline);
+        Assert.assertThrows(Exception.class, lion::doesHaveMane);
+    }
 }
